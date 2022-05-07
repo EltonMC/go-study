e09a1f8ef455506dd5b277e8619846fa4e11680c
Elton <eltonmcrvlh@gmail.com>
2022-05-07 15:45:08 -0300

    add ex-5

--- cap-3/ex-4.go
+++ cap-3/ex-4.go
@@ -5,10 +5,12 @@ import "fmt"
 type newType int
 
 var newVar newType
+var otherVar int
 
 func main() {
 	fmt.Printf("%v %T", newVar, newVar)
 	newVar = 42
 	fmt.Printf("%v %T", newVar, newVar)
-
+	otherVar = int(newVar)
+	fmt.Printf("%v %T", otherVar, otherVar)
 }
