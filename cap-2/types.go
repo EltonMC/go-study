21b0e4caa71746c2f3b8d853a882f9da2c34c6af
Elton <eltonmcrvlh@gmail.com>
2022-05-07 15:26:57 -0300

    add my types

--- cap-2/types.go
+++ cap-2/types.go
@@ -4,10 +4,19 @@ import "fmt"
 
 var x int = 10
 
+type myType int
+
+var my myType = 10
+
 func main() {
 	// static type
-	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", my)
 
 	// int, string, bool
 	// slice, array, struct, map
+
+	x = int(my)
+	fmt.Printf("%T", x)
+
 }
--- cap-2/zero.go
+++ cap-2/zero.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
-var x int = 10
+var a int
+var b float32
+var c string
+var d bool
 
 func main() {
 	// static type
-	fmt.Println(x)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 
 	// int, string, bool
 	// slice, array, struct, map
